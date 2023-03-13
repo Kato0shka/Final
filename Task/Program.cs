c297b3fdf5cbef7fdefe648e1d9fd83a86b97ee9
Ekaterina <katooshka@bk.ru>
2023-03-13 22:30:53 +0300

    Добавили изначальный массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+int n = 4;
+Console.WriteLine($"В изначальном массиве {n} элементов");
+
+string[] array = new string[n];
+array[0] = "hello";
+array[1] = "2";
+array[2] = "world";
+array[3] = ":-)";
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine($"Изначальный массив:");
+PrintArray(array);
